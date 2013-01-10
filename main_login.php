cd10cd6db369362924b4255e6c0e65c630d65f4d
fcouceiro <couceiro.f@gmail.com>
2013-01-10 01:01:50 +0000

    Style main_login updated

--- main_login.php
+++ main_login.php
@@ -3,13 +3,22 @@
 <head>
 <title>Login</title>
 	 <link href="bootstrap/css/bootstrap.css" rel="stylesheet">  
+	 <style>
+	 body {
+        padding-top: 40px; 
+      }
+	 </style>
 </head>
 <body>
-	<form class = "well" action="checklogin.php" method="post" style="text-align: center">
-		<input  name="myusername" type="text" id="myusername" PlaceHolder = "Username">	<br>	
-		<input name="mypassword" type="password" id="mypassword" PlaceHolder = "Password"><br>
-		<input class="btn btn-primary" type="submit" name="Submit" value="Login">
-	</form>
+	<div class="container" style='width:350px;>
+		<div class="well" style='background-color: #fff'>
+			<form class = "well" action="checklogin.php" method="post" style="text-align: center">
+				<input  name="myusername" type="text" id="myusername" PlaceHolder = "Username">	<br>	
+				<input name="mypassword" type="password" id="mypassword" PlaceHolder = "Password"><br>
+				<input class="btn btn-primary" type="submit" name="Submit" value="Login">
+			</form>
+		</div>
+	</div>
 </body>
 </html>
 
